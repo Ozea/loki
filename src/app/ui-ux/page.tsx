9ce15473bb864c988f562db363b74d8d4b103a2f
Alexander <alexanderosinskii@gmail.com>
2025-06-06 00:15:53 +0100

    fix: deployment

--- src/app/ui-ux/page.tsx
+++ src/app/ui-ux/page.tsx
@@ -5,17 +5,6 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 import SimpleTemplates from '@/containers/SimpleTemplates'
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.2,
-      delayChildren: 0.1,
-    },
-  },
-}
-
 const headerVariants = {
   hidden: { opacity: 0, y: 30 },
   visible: {
