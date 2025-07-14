235f489153e6041f989c74c6e57c612ddf12112a
Alexander <alexanderosinskii@gmail.com>
2025-07-14 23:51:14 +0100

    feat: restructure ui-ux page sections

---
+++ src/containers/UiUx/projects.ts
@@ -0,0 +1,44 @@
+import { Project } from './types'
+
+export const uiUxProjects: Project[] = [
+  {
+    title: 'Smarter Travel Award',
+    description:
+      'Designed a user-friendly mobile app to support the Smarter Travel programme, aimed at promoting sustainable travel for students. The app helps students plan their commute by showing live weather updates, road news, and different travel options such as cycling, driving, public transport, and carpooling with fellow students. The car-sharing feature allows drivers to earn money and helps passengers save time, encouraging smarter and more eco-friendly commuting choices.',
+    src: 'smart-travel-award',
+    href: 'https://www.behance.net/gallery/229337415/TUkit-Suistainable-Travel-UIUX',
+    hrefLabel: 'View Use Case',
+  },
+  {
+    title: 'BookShelf',
+    description:
+      'Designed a warm and inviting library app showcasing popular books, complete with descriptions and user ratings. The design focused on creating a cozy, book-like feel using soft colors, clean layouts, and consistent visual elements. The user experience was built to be intuitive and relaxing, encouraging users to explore and discover new reads.',
+    src: 'book-shelf',
+    href: 'https://www.behance.net/gallery/229336847/UIUX-Project-Library-App',
+    hrefLabel: 'View Use Case',
+  },
+]
+
+export const webDesignProjects: Project[] = [
+  {
+    title: 'AI & The environment',
+    description:
+      'Designed a web page exploring how artificial intelligence, while aimed at helping the planet, could also harm it - such as through energy use and resource demands. The project was inspired by the ideas of James Bridle.  The layout was designed for easy reading and visual impact, encouraging critical thinking through design and content.',
+    src: 'ai-and-environment',
+    href: 'https://www.behance.net/gallery/227603827/AI-The-Enviroment-landing-page',
+  },
+  {
+    title: 'Energy choices',
+    description:
+      'Designed an educational web page to explain the impact of non-renewable vs. renewable energy. Focused on clear structure, engaging visuals, and easy navigation. Included sections on energy sources, practical tips, and ways to get involved in sustainability efforts in Ireland. The design aimed to be informative, user-friendly, and visually consistent.',
+    src: 'ai-choices',
+    href: 'https://www.behance.net/gallery/227603551/Energy-Choices-Landing-Page',
+  },
+  {
+    title: 'Portfolio',
+    description:
+      'Designed a clean and modern landing page for a senior software engineer. The layout emphasises clarity and professionalism, featuring a bold hero section, structured content blocks for skills and experience, and a minimalist contact section. The design balances white space, typography, and color to reflect technical expertise and approachability.',
+    src: 'portfolio',
+    href: 'https://www.behance.net/gallery/227603317/Senior-Software-Engineer-Portfolio',
+  },
+]
